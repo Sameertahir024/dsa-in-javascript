6b769ad62aeed80c659ebc639f6e5841967ed355
sameer tahir <sameertahir024@gmail.com>
2025-04-29 16:05:17 +0500

    first comment

---
+++ array-problems/1-index.js
@@ -0,0 +1,12 @@
+//Given an array of positive integers arr[] of size n, the task is to find second largest distinct element in the array.
+
+//Note: If the second largest element does not exist, return -1.
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+// Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+// Input: arr[] = [10, 10, 10]
+// Output: -1
+// Explanation: The largest element of the array is 10 there is no second largest element.
+
+//START
